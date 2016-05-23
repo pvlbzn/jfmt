c1e43329f3e70213652f7d232dd7fbfeec1ff281
pvlbzn <pvlbzn@gmail.com>
2016-05-23 20:01:20 +0300

    Added functionality.

--- jfmt.py
+++ jfmt.py
@@ -1,19 +1,48 @@
+#! /usr/local/bin/python
+
+from __future__ import print_function
+
 import sys, os
 
-command = "java -jar ./fmt/google-java-format-1.0-all-deps.jar "
+COMMAND = "java -jar ./fmt/google-java-format-1.0-all-deps.jar "
+# Verbose output.
+DEBUG = True
+
 
 def parse_argv():
-    if len(sys.argv) == 3:
-        # First arg is always the script name,
-        # second arg is argument to the java formatter,
-        # third is a path to target file.
-        pass
     if len(sys.argv) == 2:
-        # Auto mode: second arg is a path to target.
-        # It will replace current code with a new one.
         p = os.path.abspath(sys.argv[1])
-        c = command + "--replace " + p
-        os.system(c)
+        if os.path.isfile(p):
+            format_file(p)
+        if os.path.isdir(p):
+            format_files(p)
+    if len(sys.argv) == 1:
+        p = os.getcwd()
+        format_files(p)
+
+
+def format_files(p):
+    '''
+    Format files in `p` directory.
+    '''
+    for (dirpath, dnames, fnames) in os.walk(p):
+        for fname in fnames:
+            if fname.endswith('.java'):
+                fpath = dirpath + '/' + fname
+                c = COMMAND + "--replace " + fpath
+                os.system(c)
+                if DEBUG:
+                    print("Path: ", dirpath)
+                    print("Name: ", fname)
+                    print("Done.\n")
+
+
+def format_file(p):
+    '''
+    Format the file.
+    '''
+    c = COMMAND + "--replace " + p
+    os.system(c)
 
 
 def main():
